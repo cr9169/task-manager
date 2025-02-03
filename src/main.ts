35321bb6a7dc52c7ed599dafba464ce447256178
cr9169 <bar.gabay.home@gmail.com>
2025-02-03 22:37:38 +0200

    feat: implement task list component and update app structure

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+// main.ts
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
