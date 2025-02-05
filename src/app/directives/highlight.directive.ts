64217097844a82d8276ea121f77323cf80845d12
cr9169 <bar.gabay.home@gmail.com>
2025-02-05 11:47:17 +0200

    feat: allow customizable highlight color for task items

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -3,20 +3,25 @@ import {
   ElementRef,
   HostBinding,
   HostListener,
+  Input,
 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]',
+  standalone: true,
 })
 export class HighlightDirective {
   constructor(private el: ElementRef) {}
+
+  @Input('appHighlight')
+  highlightedColor: string = 'yellow';
 
   @HostBinding('style.backgroundColor')
   backgroundColor: string = '';
 
   @HostListener('mouseenter')
   onMouseEnter() {
-    this.backgroundColor = 'yellow';
+    this.backgroundColor = this.highlightedColor;
   }
 
   @HostListener('mouseleave')
